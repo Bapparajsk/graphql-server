891f6205d6a6a70c7f9516da039109fbd07d12d1
Bapparaj sk <bapparajsk97@gmail.com>
2025-06-05 23:15:25 +0530

    feat: Configure ESLint with import plugin and rules
    This commit configures ESLint to enforce code style and import
    ordering rules. It adds the `eslint-plugin-import` plugin and
    defines rules for semi-colons, quotes, import order, duplicate
    imports, and newline after imports.  This ensures code consistency
    and readability.

---
+++ eslint.config.mjs
@@ -0,0 +1,46 @@
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import importPlugin from "eslint-plugin-import";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: {
+      js,
+      import: importPlugin // ✅ Add this
+    },
+    rules: {
+      semi: "error",
+      "quotes": ["error", "double", { avoidEscape: true }],
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+          pathGroups: [
+            {
+              pattern: "**/*.css",
+              group: "index",
+              position: "after"
+            }
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true
+          },
+          "newlines-between": "always"
+        }
+      ],
+      "import/no-duplicates": "error",
+      "import/newline-after-import": "error"
+    },
+    languageOptions: {
+      globals: globals.browser
+    },
+    extends: ["js/recommended"]
+  },
+  tseslint.configs.recommended
+]);
